892482f64ff834b26e51d4f530f6c1c8cd08434e
Sergiy Parkhomenko <sergey.parkhomenko@hotmail.com>
2025-01-27 17:49:04 +0100

    fix comment

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, Dispatch, SetStateAction } from "react";
 
 const useLocalStorage = <T>(
   key: string,
   state: T
-): [T, React.Dispatch<React.SetStateAction<T>>] => {
+): [T, Dispatch<SetStateAction<T>>] => {
   const [value, setValue] = useState<T>(() => {
     const data = localStorage.getItem(key);
     if (!data) return state;
